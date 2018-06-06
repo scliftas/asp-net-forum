321edfd9f3a8ec59ea283b95ae705ed107789417
scliftas <scliftas@gmail.com>
2018-06-06 10:43:25 +0100

    Added validation rules to thead model

---
+++ QAForum/Models/Thread.MetaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace QAForum.Models
+{
+    [MetadataType(typeof(Thread.MetaData))]
+    public partial class Thread
+    {
+        internal sealed class MetaData
+        {
+            [Required]
+            [DisplayName("Forum")]
+            public int ForumID { get; set; }
+
+            [Required]
+            [StringLength(100)]
+            [DisplayName("Title")]
+            public string ThreadTitle { get; set; }
+
+            [Required]
+            [DisplayName("Owner")]
+            public Guid? OwnerID { get; set; }
+        }
+    }
+}
