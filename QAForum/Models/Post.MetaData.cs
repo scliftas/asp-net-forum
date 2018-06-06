d2273dbf00e0f5664277dbc70893a95799fd5550
scliftas <scliftas@gmail.com>
2018-06-06 10:35:39 +0100

    Added validation rules to post model

---
+++ QAForum/Models/Post.MetaData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QAForum.Models
+{
+    [MetadataType(typeof(Post.MetaData))]
+    public partial class Post
+    {
+        internal sealed class MetaData
+        {
+            [Required]
+            [DisplayName("User")]
+            public Guid? UserID { get; set; }
+
+            [Required]
+            [DisplayName("Thread")]
+            public int? ThreadID { get; set; }
+
+            [DisplayName("Title")]
+            public string PostTitle { get; set; }
+
+            [Required]
+            [DisplayName("Posted")]
+            public DateTime PostDateTime { get; set; }
+
+            [DisplayName("Post")]
+            public string PostBody { get; set; }
+        }
+    }
+}
